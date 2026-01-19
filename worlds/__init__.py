77d084f6ef899c29c720e11fc49aa13eaf3f9fae
Nikhil Mahankali <nikhilmahankali56@gmail.com>
2026-01-19 11:08:54 -0800

    Create __init__.py for career worlds module
    Initialize the worlds module with career worlds.

---
+++ worlds/__init__.py
@@ -0,0 +1,18 @@
+"""
+Step Into My Shoes - Career Worlds
+Interactive mini-game worlds for each career path.
+"""
+
+from worlds.doctor import DoctorWorld
+from worlds.lawyer import LawyerWorld
+from worlds.influencer import InfluencerWorld
+from worlds.politician import PoliticianWorld
+from worlds.engineer import EngineerWorld
+
+__all__ = [
+    'DoctorWorld',
+    'LawyerWorld', 
+    'InfluencerWorld',
+    'PoliticianWorld',
+    'EngineerWorld'
+]
