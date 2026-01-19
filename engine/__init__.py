cde2b3877b9d6380713071db8c0eceaf9cb659b4
Nikhil Mahankali <nikhilmahankali56@gmail.com>
2026-01-19 11:09:42 -0800

    Initialize engine module with core components

---
+++ engine/__init__.py
@@ -0,0 +1,10 @@
+"""
+Step Into My Shoes - Game Engine
+Core engine components for the career simulation game.
+"""
+
+from engine.core import SceneManager
+from engine.colors import *
+from engine.ui import Button, ModernButton, CareerCard, draw_text, draw_wrapped_text
+from engine.backstory_ai import generate_backstory
+from engine.world import BaseWorld
